869e1335151399ef2ff88eb473cabf223a080b8f
CHEN PIN-RU <nezii0331@gmail.com>
2026-07-12 11:03:16 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-northeast-1_x2V0L50Ve', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2aj8qk0mi5opeqsoe6ab0tv5ec', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '6fc4uumhk7jv19k1etajpbg112', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
